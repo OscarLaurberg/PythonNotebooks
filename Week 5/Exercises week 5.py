0f75dae3146038e32c696c42871d445c40126f61
Oscar <oscar@triggerz.com>
2020-10-06 17:14:46 +0200

    exercises

--- Week 5/Exercises week 5.py
+++ Week 5/Exercises week 5.py
@@ -12,7 +12,6 @@
 #Show 2 plots in same figure: 'Married' and 'Never Married' for all ages in DK in 2020 (Hint: x axis is age from 0-125, y axis is how many people in the 2 categories). Add lengend to show names on graphs
 
 import requests
-%pylab inline
 import pandas as pd
 
 def downloadFile(url):
@@ -27,11 +26,48 @@ def downloadFile(url):
 url = 'https://api.statbank.dk/v1/data/FOLK1A/CSV?delimiter=Semicolon&Tid=2008K1%2C2020K3&CIVILSTAND=F'
 
 data = pd.read_csv(downloadFile(url), sep = ';')
-data['INDHOLD'].astype(float).pct_change()
+print(data['INDHOLD'].astype(float).pct_change())
 
 
 #Which of the 5 biggest cities has the highest percentage of 'Never Married' in 2020?
 
+url = 'https://api.statbank.dk/v1/data/FOLK1A/CSV?delimiter=Semicolon&OMR%C3%85DE=*&K%C3%98N=TOT&ALDER=IALT&CIVILSTAND=U%2CTOT&Tid=2020K3'
+df = pd.read_csv(downloadFile(url), sep = ';')
+top5_cities = ['København','Aarhus','Odense','Aalborg', 'Esbjerg']
+percentages = {}
+
+for city in top5_cities:
+    totalCitizens = df.loc[(df['OMRÅDE'] == city) & (df['CIVILSTAND'] == 'I alt')].iloc[0][5]
+    print(totalCitizens);
+    unMarried = df.loc[(df['OMRÅDE'] == city) & (df['CIVILSTAND'] == 'Ugift')].iloc[0][5]
+    percentage = (unMarried / totalCitizens) * 100
+    percentages[city] = percentage;
+
+print(percentages);
+highestPercentage = max(percentages.values())
+city = max(percentages, key=percentages.get)
+
+print('{} is the city with highest percentage of never married citizens, with {:.2f} %'.format(city,highestPercentage))
 
 #Show a bar chart of changes in marrital status in Copenhagen from 2008 till now
+
+url = 'https://api.statbank.dk/v1/data/FOLK1A/CSV?delimiter=Semicolon&OMR%C3%85DE=101&CIVILSTAND=G&Tid=2008K1%2C2009K1%2C2010K1%2C2011K1%2C2012K1%2C2013K1%2C2014K1%2C2015K1%2C2016K1%2C2017K1%2C2018K1%2C2019K1%2C2020K1'
+
+df = pd.read_csv(downloadFile(url), sep=';')
+start = '2008K1';
+#rows = df.loc[df['TID'] == start];
+marriedOrSep2008Q1 = df.loc[(df['TID'] == start)].iloc[0][3]
+values = tuple(list(df['INDHOLD']))
+time = tuple(list(df['TID']))
+valuesSubtracted = []
+
+for value in values:
+  value -= marriedOrSep2008Q1
+  valuesSubtracted.append(value);
+
+
+df_plot = pd.DataFrame({'Time':time,'Changes':valuesSubtracted})
+ax = df_plot.plot.bar(x='Time', y='Changes');
+
+
 #Show 2 plots in same figure: 'Married' and 'Never Married' for all ages in DK in 2020 (Hint: x axis is age from 0-125, y axis is how many people in the 2 categories). Add lengend to show names on graphs
